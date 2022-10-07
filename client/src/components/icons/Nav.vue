f0ca5cd3cd84f55d99e13d1ab2978e30c4e37666
Analiza <98346805+a4mejia@users.noreply.github.com>
2022-10-06 22:58:17 -0400

    vue

--- client/src/components/icons/Nav.vue
+++ client/src/components/icons/Nav.vue
@@ -1,18 +1,23 @@
-    <template>
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<script setup lang="ts">
+  import { ref } from 'vue';
+  let isActive = ref(false);
+</script>
+
+<template>
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://bulma.io">
         <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
       </a>
   
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <a :class="{'isActive':isActive}" @click= "isActive = ! isActive"  role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
   
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div id="navbarBasicExample" :class="{'isActive': isActive}">
       <div class="navbar-start">
         <a class="navbar-item">
           Home
