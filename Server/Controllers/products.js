7ea16704519ee4d425f53d3ffc683ecab99a367d
Analiza <98346805+a4mejia@users.noreply.github.com>
2022-12-01 17:57:08 -0500

    dec 1

--- Server/Controllers/products.js
+++ Server/Controllers/products.js
@@ -19,6 +19,11 @@ app
             } 
         })
         .catch.next();        
+    })
+    .post('/seed', (req,res, next)=>{
+        products.seed()
+        .then(x=> res.status(200).send(x))
+        .catch.next()
     });
 
 module.exports=app;
--- Server/models/cart.js
+++ Server/models/cart.js
@@ -1,10 +1,20 @@
 const { getProduct}= require('./products')
 const list=[];
+const {connect} = require('./mongo')
+const COLLECTION_NAME = 'cart';
 
-const get = (userId)=>{
-    return list
+async function collection(){
+    const client = await connect();
+    return client.db('chopiphy').collection(COLLECTION_NAME);
+}
+const get = async (userId)=>{
+    const db = await collection();
+    const data = await db.find((userId)).toArray();
+    return data
     .filter((cartItem)=>cartItem.userId === userId)
-    .map((cartItem)=>({ ...cartItem, product:getProduct(productId)
+    .map((cartItem)=>({ 
+        ...cartItem, 
+        product:getProduct(productId)
     }));
 };
 /**
@@ -15,15 +25,19 @@ const get = (userId)=>{
  * @returns 
  */
 const add =( userId, productId, quantity)=>{
-    let cartItem = list.find((cartItem)=>cartItem.userId=== userId && cartItem.productId === productId);
+    const db = await collection();
+    let cartItem = db.findOne((userId, productId));
+ //   let cartItem = list.find((cartItem)=>cartItem.userId=== userId && cartItem.productId === productId);
     if(cartItem){
         cartItem.quantity+= quantity;
+        db.updateOne((userId, productId), cartItem);
     }
     else{
         cartItem = {id: list.length + 1,quantity, productId: userId}
-        list.push.cartItem;
+   //     list.push.cartItem;
+        await db.insertOne(cartItem);
     }
-    return ( ...cartItem, product: getProduct(productId));
+    return {...cartItem, product:getProduct(productId)};
 };
 /**
  * 
@@ -35,13 +49,18 @@ const add =( userId, productId, quantity)=>{
 
 const update =(userId,productId, quantity)=>{
     const index= list.findIndex((cartItem)=>cartItem.userId===userId && cartItem.productId===productId);
+    const db = await collection();
+
     if(index !== -1){
         if (quantity === 0){
-            list.splice(index, 1)
+            db.deleteOne((userId, productId))
+        //   list.splice(index, 1)
             return "null";
         }
         else{
-            list[index].quantity = quantity;
+        //    list[index].quantity = quantity;
+            cartItem.quantity = quantity;
+            db.updateOne((userId, productId), cartItem);
         }
     }
     else{
